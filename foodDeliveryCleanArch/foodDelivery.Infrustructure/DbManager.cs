6c7006eaf85b226baf7f4c0c803bc4feb6fdf1a5
Alireza <aliharadi10@gmail.com>
2025-07-16 00:02:18 +0330

    add Database in Infrustructure

---
+++ foodDeliveryCleanArch/foodDelivery.Infrustructure/DbManager.cs
@@ -0,0 +1,25 @@
+﻿using foodDelivery.Domain;
+using Microsoft.EntityFrameworkCore;
+namespace foodDelivery.Infrustructure;
+
+public class DbManager : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;DataBase=FoodDB;Username=postgres;Password=Alireza1383");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<User>().HasDiscriminator<string>("Role")
+            .HasValue<Customer>("Customer")
+            .HasValue<Vendor>("Vendor")
+            .HasValue<Admin>("Admin");
+    }
+    
+}
---
+++ foodDeliveryCleanArch/foodDelivery.Infrustructure/Migrations/20250715114336_Init2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace foodDelivery.Infrustructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
