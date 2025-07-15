9573355f1f5e415109bb2e2695da5325613a7e99
Alireza <aliharadi10@gmail.com>
2025-07-16 00:04:49 +0330

    add CustomerController

---
+++ foodDeliveryCleanArch/foodDelivery.Pres/Controller/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using foodDelivery.Application;
+using foodDelivery.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace foodDelivery.Pres.Controller;
+
+[ApiController]
+[Route("customer")]
+public class CustomerController(ICustomerService customerService) : ControllerBase
+{
+    [HttpPost("signup")]
+    public IActionResult Signup([FromBody] CustomerSignupRequest request)
+    {
+        try
+        {
+            var response = customerService.Signup(request);
+            return Ok(response);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(
+              new { error = e.Message }
+              );
+        }
+    }
+}
