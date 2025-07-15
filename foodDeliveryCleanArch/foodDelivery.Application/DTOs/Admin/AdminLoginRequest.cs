def0f480e64e2e44679c84fbc6e2e5728443388a
Alireza <aliharadi10@gmail.com>
2025-07-16 02:37:36 +0330

    add AdminLogin DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Admin/AdminLoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace foodDelivery.Application.DTOs.Admin;
+
+public class AdminLoginRequest(string username, string password)
+{
+    public string Username { get; set; } = username;
+    public string Password { get; set; } = password;
+}
