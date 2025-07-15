94bd3b0d6d274eb3947462d9339c4275ff5fffc2
Alireza <aliharadi10@gmail.com>
2025-07-15 15:38:32 +0330

    Add User class in Domain

---
+++ foodDeliveryCleanArch/foodDelivery.Domain/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace foodDelivery.Domain;
+
+public abstract class User
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+
+    protected User(string username, string password, string role)
+    {
+        Id = Guid.NewGuid();
+        Username = username;
+        Password = password;
+        Role = role;
+    }
+}
