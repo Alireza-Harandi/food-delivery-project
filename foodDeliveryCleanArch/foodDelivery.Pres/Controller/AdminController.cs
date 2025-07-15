ad9b716bbda3e78329ff045f17320d3ebd037b6b
Alireza <aliharadi10@gmail.com>
2025-07-16 02:06:56 +0330

    add AdminController class

---
+++ foodDeliveryCleanArch/foodDelivery.Pres/Controller/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using foodDelivery.Application.DTOs.Admin;
+using foodDelivery.Application.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace foodDelivery.Pres.Controller;
+
+[ApiController]
+[Route("admin")]
+public class AdminController(IAdminService adminService) : ControllerBase
+{
+    [Authorize]
+    [HttpPost("signup")]
+    public IActionResult Signup([FromBody] AdminSignupRequest request)
+    {
+        try
+        {
+            var response = adminService.Signup(request);
+            return Ok(response);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            return Unauthorized(new { error = e.Message });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { error = e.Message });
+        }
+    }
+}
