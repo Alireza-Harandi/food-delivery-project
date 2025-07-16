451a5fa566159c2d3218e00d4796b5d60428179a
Alireza <aliharadi10@gmail.com>
2025-07-17 00:50:54 +0330

    delete admin/customer Login DTO

--- foodDeliveryCleanArch/foodDelivery.Application/DTOs/Admin/AdminLoginRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace foodDelivery.Application.DTOs.Admin;
-
-public class AdminLoginRequest(string username, string password)
-{
-    public string Username { get; set; } = username;
-    public string Password { get; set; } = password;
-}
--- foodDeliveryCleanArch/foodDelivery.Application/DTOs/Admin/AdminLoginResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace foodDelivery.Application.DTOs.Admin;
-
-public class AdminLoginResponse(string token)
-{
-    public string Token { get; set; } = token;
-}
