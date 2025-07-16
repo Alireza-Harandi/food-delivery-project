b52e8afc8f79d379ce0b35afd39c5770e5cbd7aa
Alireza <aliharadi10@gmail.com>
2025-07-17 00:55:55 +0330

    delete login methode/api from controller and service in customer and admin

--- foodDeliveryCleanArch/foodDelivery.Application/Interface/IAdminService.cs
+++ foodDeliveryCleanArch/foodDelivery.Application/Interface/IAdminService.cs
@@ -5,5 +5,4 @@ namespace foodDelivery.Application.Interface;
 public interface IAdminService
 {
     AdminSignupResponse Signup(AdminSignupRequest request);
-    AdminLoginResponse Login(AdminLoginRequest request);
 }
--- foodDeliveryCleanArch/foodDelivery.Application/Interface/ICustomerService.cs
+++ foodDeliveryCleanArch/foodDelivery.Application/Interface/ICustomerService.cs
@@ -5,5 +5,4 @@ namespace foodDelivery.Application.Interface;
 public interface ICustomerService
 {
     CustomerSignupResponse Signup(CustomerSignupRequest request);
-    CustomerLoginResponse Login(CustomerLoginRequest request);
 }
