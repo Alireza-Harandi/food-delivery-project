41f947a4f516e6552c7cc024f826e726bdc9a334
Alireza <aliharadi10@gmail.com>
2025-07-17 02:03:43 +0330

    add Location class for attribute of vendor class

---
+++ foodDeliveryCleanArch/foodDelivery.Domain/Location.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace foodDelivery.Domain;
+
+[Owned]
+public class Location
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Address { get; set; }
+
+    public Location(double latitude, double longitude, string address)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+        Address = address;
+    }
+}
