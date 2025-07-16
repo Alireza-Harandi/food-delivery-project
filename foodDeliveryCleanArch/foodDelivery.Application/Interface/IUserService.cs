a52e7ea9c5e065fdd01833cdb476fc027d2a42db
Alireza <aliharadi10@gmail.com>
2025-07-17 00:57:10 +0330

    add UserService - interface & class

---
+++ foodDeliveryCleanArch/foodDelivery.Application/Interface/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using foodDelivery.Application.DTOs.User;
+
+namespace foodDelivery.Application.Interface;
+
+public interface IUserService
+{
+    UserLoginResponse Login(UserLoginRequest request);
+}
