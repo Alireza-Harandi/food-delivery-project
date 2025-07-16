b55afb288f86fca0429635c5bd74a6c98285ac29
Alireza <aliharadi10@gmail.com>
2025-07-17 00:49:42 +0330

    update related project files

--- foodDeliveryCleanArch/foodDelivery.Application/obj/Debug/net8.0/foodDelivery.Application.AssemblyInfo.cs
+++ foodDeliveryCleanArch/foodDelivery.Application/obj/Debug/net8.0/foodDelivery.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodDelivery.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81183e8bc1177a0db5af4b482dc7dd3806b884d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a89cfd137e465953486e4ae182aa3d1e09b01af")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodDelivery.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodDelivery.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
