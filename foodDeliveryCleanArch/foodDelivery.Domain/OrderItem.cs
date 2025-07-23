87011fe3ef03878e08d3ec2bfae4fb3bbca8c8f6
Alireza <aliharadi10@gmail.com>
2025-07-23 22:08:51 +0330

    add orderItem class in domain

---
+++ foodDeliveryCleanArch/foodDelivery.Domain/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace foodDelivery.Domain;
+
+public class OrderItem
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public Guid OrderId { get; set; }
+    [ForeignKey("OrderId")]
+    public Order? Order { get; set; }
+
+    public Guid FoodId { get; set; }
+    [ForeignKey("FoodId")]
+    public Food? Food { get; set; }
+    public int Quantity { get; set; }
+
+    public OrderItem(Guid orderId, Guid foodId, int quantity)
+    {
+        Id = Guid.NewGuid();
+        OrderId = orderId;
+        FoodId = foodId;
+        Quantity = quantity;
+    }
+
+    public OrderItem()
+    {
+    }
+}
