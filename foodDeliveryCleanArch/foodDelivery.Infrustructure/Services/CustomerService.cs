1c4d8f69f70f5c727e6937b15da1d90ce265f83a
Alireza <aliharadi10@gmail.com>
2025-07-23 22:59:44 +0330

    add SetOrderQuantity method in CustomerService

--- foodDeliveryCleanArch/foodDelivery.Infrustructure/Services/CustomerService.cs
+++ foodDeliveryCleanArch/foodDelivery.Infrustructure/Services/CustomerService.cs
@@ -66,11 +66,62 @@ public class CustomerService(DbManager dbManager, IAuthService authService) : IC
             request.Quantity
         );
         dbManager.OrderItems.Add(orderItem);
+        order.Total = CalculateTotalPrice(order.Id);
         dbManager.SaveChanges();
 
         return new AddToOrderResponse(
             order.Id,
             orderItem.Id
         );
+    }
+
+    private double CalculateTotalPrice(Guid orderId)
+    {
+        List<OrderItem> orderItems = dbManager.OrderItems
+            .Include(o => o.Food)
+            .Where(o => o.OrderId == orderId)
+            .ToList();
+
+        double total = 0;
+        foreach (var orderItem in orderItems)
+        {
+            total += orderItem.Food!.Price * orderItem.Quantity;
+        }
+
+        return total;
+    }
+
+    public void SetOrderQuantity(SetOrderQuantityDto request)
+    {
+        Token token = CheckAccess();
+        if (request.GetType().GetProperties().Any(p => p.GetValue(request) == null))
+            throw new ArgumentException("All fields are required");
+        Customer customer = dbManager.Customers.FirstOrDefault(c => c.UserId == token.UserId)!;
+        
+        OrderItem? orderItem = dbManager.OrderItems
+            .Include(i => i.Order)
+            .FirstOrDefault(i =>
+                i.Id == request.OrderItemId);
+        
+        Order? order = dbManager.Orders
+            .Include(o => o.Items)
+            .FirstOrDefault(o => o.Id == request.OrderId);
+        
+        if (order == null)
+            throw new KeyNotFoundException("Order not found");
+        if (orderItem == null)
+            throw new KeyNotFoundException("OrderItem not found");
+        if (orderItem.Order!.CustomerId != customer.Id)
+            throw new UnauthorizedAccessException("Customer is not access");
+
+        if (request.Quantity == 0)
+        {
+            dbManager.OrderItems.Remove(orderItem);
+            if (order.Items.Count == 0)
+                dbManager.Orders.Remove(order);
+        }
+        else
+            orderItem.Quantity = request.Quantity;
+        dbManager.SaveChanges();
     }
 }
