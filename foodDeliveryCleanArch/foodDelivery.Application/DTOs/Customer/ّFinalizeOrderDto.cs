94a71344f373f1bdbc8e3d64f9f0ec8f9f9d5399
Alireza <aliharadi10@gmail.com>
2025-07-24 02:23:13 +0330

    add FinalizeOrder DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Customer/ّFinalizeOrderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace foodDelivery.Application.DTOs.Customer;
+
+public class FinalizeOrderRequest(Guid orderId, double latitude, double longitude, string address)
+{
+    public Guid OrderId { get; set; } = orderId;
+    public double Latitude { get; set; } = latitude;
+    public double Longitude { get; set; } = longitude;
+    public string Address { get; set; } = address;
+}
+
+public class FinalizeOrderResponse(Guid orderId)
+{
+    public Guid OrderId { get; set; } = orderId;
+}
