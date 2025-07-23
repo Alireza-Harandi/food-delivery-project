126d152fa564af261d6630fad7f3de687157abcb
Alireza <aliharadi10@gmail.com>
2025-07-24 01:41:54 +0330

    add CustomerOrder DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Customer/CustomerOrderDto.cs
@@ -0,0 +1,19 @@
+﻿using foodDelivery.Domain;
+
+namespace foodDelivery.Application.DTOs.Customer;
+
+public class CustomerOrderDto(Guid orderId, Guid restaurantId, OrderStatus orderStatus, double totalPrice, List<OrderItemDto> orderItems)
+{
+    public Guid OrderId { get; set; } = orderId;
+    public Guid RestaurantId { get; set; } = restaurantId;
+    public OrderStatus OrderStatus { get; set; } = orderStatus;
+    public double TotalPrice { get; set; } = totalPrice;
+    public List<OrderItemDto> OrderItems { get; set; } = orderItems;
+}
+
+public class OrderItemDto(Guid orderItemId, Guid foodId, int quantity)
+{
+    public Guid OrderItemId { get; set; }
+    public Guid FoodId { get; set; }
+    public int Quantity { get; set; }
+}
