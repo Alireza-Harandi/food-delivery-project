76f64211e6bb85f6fa12e8557f5f4890ec70a475
Alireza <aliharadi10@gmail.com>
2025-07-23 18:26:08 +0330

    add AutocompleteResponse DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Customer/AutocompleteResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace foodDelivery.Application.DTOs.Customer;
+
+public class AutocompleteResponseDto(List<AutocompleteItemDto> items)
+{
+    public List<AutocompleteItemDto> Item { get; set; } = items;
+}
+
+public class AutocompleteItemDto(Guid id, string name)
+{
+    public Guid Id { get; set; } = id;
+    public string Name { get; set; } = name;
+}
+
+
+
+
