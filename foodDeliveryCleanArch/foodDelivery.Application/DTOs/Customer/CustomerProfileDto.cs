27f4635b9816bd084423b1945d79ac0d31af0998
Alireza <aliharadi10@gmail.com>
2025-07-24 04:34:45 +0330

    add CustomerProfile DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Customer/CustomerProfileDto.cs
@@ -0,0 +1,8 @@
+﻿namespace foodDelivery.Application.DTOs.Customer;
+
+public class CustomerProfileDto(Guid customerId, string name, string phoneNumber)
+{
+    public Guid CustomerId { get; set; } = customerId;
+    public string Name { get; set; } = name;
+    public string PhoneNumber { get; set; } = phoneNumber;
+}
