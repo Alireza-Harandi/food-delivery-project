b2f04930baa41fd52b9958f1321a01346eb878f6
Alireza <aliharadi10@gmail.com>
2025-07-24 16:17:21 +0330

    add Reports DTO

---
+++ foodDeliveryCleanArch/foodDelivery.Application/DTOs/Admin/ReportsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace foodDelivery.Application.DTOs.Admin;
+
+public class ReportsDto(List<ReportDetails> reports)
+{
+    List<ReportDetails> Reposts { get; set; } = reports;
+}
+
+public class ReportDetails(Guid reportId, Guid customerId, Guid restaurantId, string customerName, string restaurantName, DateTime date, string description)
+{
+    public Guid ReportId { get; set; } = reportId;
+    public Guid CustomerId { get; set; } = customerId;
+    public Guid RestaurantId { get; set; } = restaurantId;
+    public string CustomerName { get; set; } = customerName;
+    public string RestaurantName { get; set; } = restaurantName;
+    public DateTime Date { get; set; } = date;
+    public string Description { get; set; } = description;
+}
