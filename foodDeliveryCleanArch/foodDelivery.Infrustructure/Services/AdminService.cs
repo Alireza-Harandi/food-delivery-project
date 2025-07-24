ccf8cd4134a1e81c5d7678aa28848f0d86b4f808
Alireza <aliharadi10@gmail.com>
2025-07-24 16:17:41 +0330

    add GetReports method in AdminService

--- foodDeliveryCleanArch/foodDelivery.Infrustructure/Services/AdminService.cs
+++ foodDeliveryCleanArch/foodDelivery.Infrustructure/Services/AdminService.cs
@@ -1,6 +1,7 @@
 ﻿using foodDelivery.Application.DTOs.Admin;
 using foodDelivery.Application.Interface;
 using foodDelivery.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace foodDelivery.Infrustructure.Services;
 
@@ -13,22 +14,41 @@ public class AdminService(DbManager dbManager, IAuthService authService) : IAdmi
             throw new UnauthorizedAccessException("admin not found");
         return token;
     }
-    
+
     public AdminSignupResponse Signup(AdminSignupRequest request)
     {
         CheckAccess();
         if (request.GetType().GetProperties().Any(p => p.GetValue(request) == null))
             throw new ArgumentException("All fields are required");
-        if (dbManager.Users.Any(u => u.Username == request.Username) )
+        if (dbManager.Users.Any(u => u.Username == request.Username))
             throw new ArgumentException("Username already taken");
-        
+
         User user = new User(request.Username, request.Password, Role.Admin);
         dbManager.Users.Add(user);
         dbManager.SaveChanges();
-        
+
         return new AdminSignupResponse(
             user.Username,
             user.Password
-            );
+        );
+    }
+
+    public ReportsDto GetReports()
+    {
+        CheckAccess();
+        List<ReportDetails> reports = dbManager.Reports
+            .Include(r => r.Customer)
+            .Include(r => r.Restaurant)
+            .Select(r => new ReportDetails(
+                r.Id,
+                r.CustomerId,
+                r.RestaurantId,
+                r.Customer!.Name,
+                r.Restaurant!.Name,
+                r.Date,
+                r.Description
+            )).ToList();
+
+        return new ReportsDto(reports);
     }
 }
